a80c9dcc0c1a81e2d4f698b8fdcc53308cde534f
amirmb <batbeginss@gmail.com>
2024-10-18 10:39:49 +0330

    sec initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import AddArticle from "./pages/addArticle/AddArticle";
@@ -9,15 +9,14 @@ import Article from "./pages/article/Article";
 function App() {
   return (
     <>
-     
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/add-article" element={<AddArticle />} />
-          <Route path="/article/:id" element={<Article />} />
-          <Route path="/edit-article/:id" element={<EditArticle />} />
-        </Routes>
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/add-article" element={<AddArticle />} />
+        <Route path="/article/:id" element={<Article />} />
+        <Route path="/edit-article/:id" element={<EditArticle />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </>
   );
 }
