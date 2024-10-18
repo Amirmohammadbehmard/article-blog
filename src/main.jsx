cd26ddabc6640e26cc7d08824ce58e22b6719305
amirmb <batbeginss@gmail.com>
2024-10-18 11:14:39 +0330

    initial 6

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {  HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import MyNavbar from "./components/navbar/MyNavbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
-  <HashRouter>
-    <MyNavbar />
-        <App />
-    </HashRouter>
+    <BrowserRouter>
+      <MyNavbar />
+      <App />
+    </BrowserRouter>
   </>
 );
